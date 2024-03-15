82c6018c56196ae76bbda53684ad5c12c61557ba
Pranav Pranjal <pranavpranjal01@gmail.com>
2024-03-15 19:37:38 +0530

    DiamondStarPatternAdded

---
+++ src/DiamondStarPattern.java
@@ -0,0 +1,45 @@
+/*
+     *
+    ***
+   *****
+  *******
+ *********
+ *********
+  *******
+   *****
+    ***
+     *
+ */
+public class DiamondStarPattern {
+    public static void main(String[] args) {
+        Pattern(5);
+
+    }
+    public static void Pattern(int n){
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j < n-i; j++) {
+                System.out.print(" ");
+
+            }
+            for (int j = 0; j <2*i -1 ; j++) {
+                System.out.print("*");
+
+            }
+            System.out.println();
+
+        }
+        for (int i = n; i >=1 ; i--) {
+            for (int j = 0; j < n-i; j++) {
+                System.out.print(" ");
+
+            }
+            for (int j = 0; j < 2*i -1; j++) {
+                System.out.print("*");
+
+            }
+            System.out.println();
+
+
+        }
+    }
+}
