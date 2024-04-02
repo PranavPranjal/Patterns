353725c867a04d661c9d32ec6a3f77465eec847d
Pranav Pranjal <pranavpranjal01@gmail.com>
2024-04-02 22:19:40 +0530

    SymmetricVoidPatternAdded

---
+++ src/SymmetricVoidPattern.java
@@ -0,0 +1,18 @@
+/*
+ ******
+ **  **
+ *    *
+ *    *
+ **  **
+ ******
+*/
+public class SymmetricVoidPattern {
+    public static void main(String[] args) {
+        Pattern(3);
+
+    }
+    public static void Pattern(int n){
+
+        }
+    }
+}
